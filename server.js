5f85dd38111a3c6b122e01de9797732c72d0ec1a
GRIFFnDOOR <Griff.howl@gmail.com>
2013-11-24 19:40:09 -0800

    serving of html competition pages and competition database

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ var _ = require('underscore');
 
 var app = express()
 	.set('views', __dirname + '/public/htm')
+	.set("view engine", "hbs")
 	.use(express.static(__dirname + '/public'))
 	.use(express.static(__dirname + '/public/htm'))
-	.use(express.static(__dirname +'/bower_components'))
+	.use(express.static(__dirname + '/bower_components'))
 	.use(express.bodyParser())	
 
 var db = { // temporary in-memory database
-	first: {id:2}
+	test: {
+	  "name": "Test",
+	  "sport": "Soccer",
+	  "team-count": "0",
+	  "security": "Public",
+	  "teams": {},
+	  "system": "Single-Elimination",
+	  "id": 3
+	}
 }; 
 
 app.get('/competitions', function(req,res) {
@@ -21,11 +30,15 @@ app.get('/competitions', function(req,res) {
 // Given a name or id, serves either an html page a json model 
 app.get('/competitions/:nameOrId', function(req,res) {
 	var nameOrId = req.param('nameOrId'); //find compotition by ID or Name
-	var comp = _.findWhere(_.values(db), {id:nameOrId}) || db[nameOrId];
+	var comp = _.findWhere(_.values(db), {id:parseInt(nameOrId)}) || db[nameOrId];
+
+	console.log(req.accepted);
+	console.log(_.findWhere(req.accepted, {subtype:'html'}));
 
 	if (!comp) res.send(404, 'Sorry, competition "' + nameOrId + '" doesn\'t exist');
-	else if (req.accepts('json')) res.json(comp);
-	else if (req.accepts('html')) ; //TODO: render HTML
+	// only resolves to true if json is explicitly in the accept header (no wildcards)
+	else if (_.findWhere(req.accepted, {subtype:'json'})) res.json(comp);
+	else if (_.findWhere(req.accepted, {subtype:'html'})) res.render('competition.hbs', comp); 
 	else res.send(406, 'Not Acceptable');
 });
 
@@ -33,10 +46,10 @@ app.post('/competitions', function(req,res) {
 	// only works if 1+ competitions exist in the db
 	req.body.id = _.max(_.pluck(_.values(db),'id')) +1;
 	db[req.body.name.replace(/\W+/g, '-')] = req.body;
-	res.json(req.body); 
+	res.json(req.body);
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port);
-console.log(__filename + ' is now listening on port' + port);
+console.log(__filename + ' is now listening on port ' + port);
 
